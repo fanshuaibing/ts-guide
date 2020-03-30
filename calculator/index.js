83901d37b4cc4f066bb5bac659a956c3edc4258c
fatfan <fatfan1020@gmail.com>
2020-03-30 20:25:27 +0800

    ts 使用 class 重构计算器

--- calculator/index.js
+++ calculator/index.js
@@ -1,94 +1,114 @@
 {
-    function createButton(text, btnContainer, className) {
-        var button = document.createElement("button");
-        button.textContent = text;
-        className && button.classList.add(className);
-        btnContainer.appendChild(button);
-    }
-    var container_1 = document.createElement("div");
-    container_1.classList.add("container");
-    document.body.appendChild(container_1);
-    var outputContainer = document.createElement("div");
-    outputContainer.classList.add("output-container");
-    container_1.appendChild(outputContainer);
-    var output_1 = document.createElement("span");
-    output_1.classList.add("output");
-    output_1.textContent = "0";
-    outputContainer.appendChild(output_1);
-    var arr = [
-        ["Clear", "÷"],
-        ["7", "8", "9", "×"],
-        ["4", "5", "6", "-"],
-        ["1", "2", "3", "+"],
-        ["0", ".", "="],
-    ];
-    arr.forEach(function (textList) {
-        var btnContainer = document.createElement("div");
-        btnContainer.classList.add("row");
-        textList.forEach(function (text) {
-            createButton(text, btnContainer, "text-" + text);
-        });
-        container_1.append(btnContainer);
-    });
-    var n1_1 = "";
-    var n2_1 = "";
-    var operator_1 = "";
-    var result_1 = "";
-    container_1.addEventListener("click", function (event) {
-        if (event.target instanceof HTMLButtonElement) {
-            var text = event.target.textContent;
-            if ("1234567890.".indexOf(text) >= 0) {
-                if (operator_1) {
-                    n2_1 += text;
-                    output_1.textContent = n2_1;
-                }
-                else {
-                    result_1 = "";
-                    n1_1 += text;
-                    output_1.textContent = n1_1;
-                }
-            }
-            else if ("+×-÷".indexOf(text) >= 0) {
-                operator_1 = text;
-                if (result_1) {
-                    n1_1 = result_1;
-                    result_1 = "";
-                }
-            }
-            else if ("=".indexOf(text) >= 0) {
-                var newN1 = parseFloat(n1_1);
-                var newN2 = parseFloat(n2_1);
-                var midResult = void 0;
-                if (operator_1 === "+") {
-                    midResult = newN1 + newN2;
-                }
-                else if (operator_1 === "-") {
-                    midResult = newN1 - newN2;
-                }
-                else if (operator_1 === "×") {
-                    midResult = newN1 * newN2;
-                }
-                else if (operator_1 === "÷") {
-                    midResult = newN1 / newN2;
-                }
-                if (midResult === Infinity && operator_1 === "÷") {
-                    result_1 = "除数不能为零";
-                }
-                else {
-                    result_1 = midResult.toString().slice(0, 25);
-                }
-                n1_1 = "";
-                n2_1 = "";
-                operator_1 = "";
-                output_1.textContent = result_1;
-            }
-            else if ("Clear".indexOf(text) >= 0) {
-                n1_1 = "";
-                n2_1 = "";
-                operator_1 = "";
-                result_1 = "";
-                output_1.textContent = "0";
-            }
+    var Calculator = /** @class */ (function () {
+        function Calculator() {
+            this.n1 = "";
+            this.n2 = "";
+            this.operator = "";
+            this.result = "";
+            this.arr = [
+                ["Clear", "÷"],
+                ["7", "8", "9", "×"],
+                ["4", "5", "6", "-"],
+                ["1", "2", "3", "+"],
+                ["0", ".", "="],
+            ];
+            this.createContainer();
+            this.createOutput();
+            this.createButtons();
+            this.bindEvents();
         }
-    });
+        Calculator.prototype.createContainer = function () {
+            this.container = document.createElement("div");
+            this.container.classList.add("container");
+            document.body.appendChild(this.container);
+        };
+        Calculator.prototype.createOutput = function () {
+            this.outputContainer = document.createElement("div");
+            this.outputContainer.classList.add("output-container");
+            this.container.appendChild(this.outputContainer);
+            this.output = document.createElement("span");
+            this.output.classList.add("output");
+            this.output.textContent = "0";
+            this.outputContainer.appendChild(this.output);
+        };
+        Calculator.prototype.createButtons = function () {
+            var _this = this;
+            this.arr.forEach(function (textList) {
+                var btnContainer = document.createElement("div");
+                btnContainer.classList.add("row");
+                textList.forEach(function (text) {
+                    _this.createButton(text, btnContainer, "text-" + text);
+                });
+                _this.container.append(btnContainer);
+            });
+        };
+        Calculator.prototype.createButton = function (text, btnContainer, className) {
+            var button = document.createElement("button");
+            button.textContent = text;
+            className && button.classList.add(className);
+            btnContainer.appendChild(button);
+        };
+        Calculator.prototype.bindEvents = function () {
+            var _this = this;
+            this.container.addEventListener("click", function (event) {
+                if (event.target instanceof HTMLButtonElement) {
+                    var text = event.target.textContent;
+                    if ("1234567890.".indexOf(text) >= 0) {
+                        if (_this.operator) {
+                            _this.n2 += text;
+                            _this.output.textContent = _this.n2;
+                        }
+                        else {
+                            _this.result = "";
+                            _this.n1 += text;
+                            _this.output.textContent = _this.n1;
+                        }
+                    }
+                    else if ("+×-÷".indexOf(text) >= 0) {
+                        _this.operator = text;
+                        if (_this.result) {
+                            _this.n1 = _this.result;
+                            _this.result = "";
+                        }
+                    }
+                    else if ("=".indexOf(text) >= 0) {
+                        var newN1 = parseFloat(_this.n1);
+                        var newN2 = parseFloat(_this.n2);
+                        var midResult = void 0;
+                        if (_this.operator === "+") {
+                            midResult = newN1 + newN2;
+                        }
+                        else if (_this.operator === "-") {
+                            midResult = newN1 - newN2;
+                        }
+                        else if (_this.operator === "×") {
+                            midResult = newN1 * newN2;
+                        }
+                        else if (_this.operator === "÷") {
+                            midResult = newN1 / newN2;
+                        }
+                        if (midResult === Infinity && _this.operator === "÷") {
+                            _this.result = "除数不能为零";
+                        }
+                        else {
+                            _this.result = midResult.toString().slice(0, 25);
+                        }
+                        _this.n1 = "";
+                        _this.n2 = "";
+                        _this.operator = "";
+                        _this.output.textContent = _this.result;
+                    }
+                    else if ("Clear".indexOf(text) >= 0) {
+                        _this.n1 = "";
+                        _this.n2 = "";
+                        _this.operator = "";
+                        _this.result = "";
+                        _this.output.textContent = "0";
+                    }
+                }
+            });
+        };
+        return Calculator;
+    }());
+    new Calculator();
 }
--- calculator/index.ts
+++ calculator/index.ts
@@ -1,97 +1,123 @@
 {
-  function createButton(
-    text: string,
-    btnContainer: HTMLDivElement,
-    className: string
-  ) {
-    let button: HTMLButtonElement = document.createElement("button");
-    button.textContent = text;
-    className && button.classList.add(className);
-    btnContainer.appendChild(button);
-  }
+  class Calculator {
+    public n1: string = "";
+    public n2: string = "";
+    public operator: string = "";
+    public result: string = "";
+    public container: HTMLDivElement;
+    public outputContainer: HTMLDivElement;
+    public output: HTMLSpanElement;
+    public arr: Array<Array<string>> = [
+      ["Clear", "÷"],
+      ["7", "8", "9", "×"],
+      ["4", "5", "6", "-"],
+      ["1", "2", "3", "+"],
+      ["0", ".", "="],
+    ];
 
-  let container: HTMLDivElement = document.createElement("div");
-  container.classList.add("container");
-  document.body.appendChild(container);
-
-  let outputContainer: HTMLDivElement = document.createElement("div");
-  outputContainer.classList.add("output-container");
-  container.appendChild(outputContainer);
-
-  let output: HTMLSpanElement = document.createElement("span");
-  output.classList.add("output");
-  output.textContent = "0";
-  outputContainer.appendChild(output);
-
-  let arr: Array<Array<string>> = [
-    ["Clear", "÷"],
-    ["7", "8", "9", "×"],
-    ["4", "5", "6", "-"],
-    ["1", "2", "3", "+"],
-    ["0", ".", "="],
-  ];
-  arr.forEach((textList: Array<string>) => {
-    let btnContainer: HTMLDivElement = document.createElement("div");
-    btnContainer.classList.add("row");
-    textList.forEach((text: string) => {
-      createButton(text, btnContainer, `text-${text}`);
-    });
-    container.append(btnContainer);
-  });
-  let n1: string = "";
-  let n2: string = "";
-  let operator: string = "";
-  let result: string = "";
-
-  container.addEventListener("click", (event) => {
-    if (event.target instanceof HTMLButtonElement) {
-      const text = event.target.textContent;
-      if ("1234567890.".indexOf(text) >= 0) {
-        if (operator) {
-          n2 += text;
-          output.textContent = n2;
-        } else {
-          result = "";
-          n1 += text;
-          output.textContent = n1;
-        }
-      } else if ("+×-÷".indexOf(text) >= 0) {
-        operator = text;
-        if (result) {
-          n1 = result;
-          result = "";
-        }
-      } else if ("=".indexOf(text) >= 0) {
-        let newN1: number = parseFloat(n1);
-        let newN2: number = parseFloat(n2);
-        let midResult: number;
-
-        if (operator === "+") {
-          midResult = newN1 + newN2;
-        } else if (operator === "-") {
-          midResult = newN1 - newN2;
-        } else if (operator === "×") {
-          midResult = newN1 * newN2;
-        } else if (operator === "÷") {
-          midResult = newN1 / newN2;
-        }
-
-        if (midResult === Infinity && operator === "÷") {
-          result = "除数不能为零";
-        } else {
-          result = midResult.toString().slice(0, 25);
-        }
-        n1 = "";
-        n2 = "";
-        operator = "";
-        output.textContent = result;
-      } else if ("Clear".indexOf(text) >= 0) {
-        n1 = "";
-        n2 = "";
-        operator = "";
-        result = "";
-        output.textContent = "0";
-      }
+    constructor() {
+      this.createContainer();
+      this.createOutput();
+      this.createButtons();
+      this.bindEvents();
     }
-  });
+
+    createContainer() {
+      this.container = document.createElement("div");
+      this.container.classList.add("container");
+      document.body.appendChild(this.container);
+    }
+
+    createOutput() {
+      this.outputContainer = document.createElement("div");
+      this.outputContainer.classList.add("output-container");
+      this.container.appendChild(this.outputContainer);
+
+      this.output = document.createElement("span");
+      this.output.classList.add("output");
+      this.output.textContent = "0";
+      this.outputContainer.appendChild(this.output);
+    }
+
+    createButtons() {
+      this.arr.forEach((textList: Array<string>) => {
+        let btnContainer: HTMLDivElement = document.createElement("div");
+        btnContainer.classList.add("row");
+        textList.forEach((text: string) => {
+          this.createButton(text, btnContainer, `text-${text}`);
+        });
+        this.container.append(btnContainer);
+      });
+    }
+
+    createButton(text: string, btnContainer: HTMLDivElement, className: string) {
+      let button: HTMLButtonElement = document.createElement("button");
+      button.textContent = text;
+      className && button.classList.add(className);
+      btnContainer.appendChild(button);
+    }
+
+    bindEvents() {
+      this.container.addEventListener("click", (event) => {
+        if (event.target instanceof HTMLButtonElement) {
+          const text = event.target.textContent;
+          if ("1234567890.".indexOf(text) >= 0) {
+            if (this.operator) {
+              this.n2 += text;
+              this.output.textContent = this.n2;
+            }
+            else {
+              this.result = "";
+              this.n1 += text;
+              this.output.textContent = this.n1;
+            }
+          }
+          else if ("+×-÷".indexOf(text) >= 0) {
+            this.operator = text;
+            if (this.result) {
+              this.n1 = this.result;
+              this.result = "";
+            }
+          }
+          else if ("=".indexOf(text) >= 0) {
+            let newN1: number = parseFloat(this.n1);
+            let newN2: number = parseFloat(this.n2);
+            let midResult: number;
+
+            if (this.operator === "+") {
+              midResult = newN1 + newN2;
+            }
+            else if (this.operator === "-") {
+              midResult = newN1 - newN2;
+            }
+            else if (this.operator === "×") {
+              midResult = newN1 * newN2;
+            }
+            else if (this.operator === "÷") {
+              midResult = newN1 / newN2;
+            }
+
+            if (midResult === Infinity && this.operator === "÷") {
+              this.result = "除数不能为零";
+            }
+            else {
+              this.result = midResult.toString().slice(0, 25);
+            }
+            this.n1 = "";
+            this.n2 = "";
+            this.operator = "";
+            this.output.textContent = this.result;
+          }
+          else if ("Clear".indexOf(text) >= 0) {
+            this.n1 = "";
+            this.n2 = "";
+            this.operator = "";
+            this.result = "";
+            this.output.textContent = "0";
+          }
+        }
+      });
+    }
+  }
+  new Calculator();
 }
